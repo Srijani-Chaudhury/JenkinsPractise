eb948bfe4403c97eda04a7850def49698601d914
SrijaniHehe <srijanicompetitions@gmail.com>
2024-06-04 12:44:10 +0530

    Changes

--- src/Post.js
+++ src/Post.js
@@ -3,17 +3,23 @@ import React, { useState } from 'react'
 export default function Post() {
 
     const [post,setPost]=useState();
+    const [count,setCount]=useState(1);
     const onClickHander = () => {
         fetch('https://jsonplaceholder.typicode.com/posts/1').then(response => response.json()).then(json => {
             setPost(json);
             console.log(json);
         })
     };
+
+    const onCountHandler = () => {
+        setCount(count++);
+    };
     return (
         <div>
 
             <p>ID : {post? post.id:null}</p>
             <button onClick={onClickHander}>Get Post </button>
+            <button onClick={onCountHandler}>Get Post </button>
         </div>
     )
 }
